0507e0d3bdbce26d9e7246eba0648d7176e2b57f
Jonitha42 <yonipardo4@gmail.com>
2021-01-31 00:02:01 -0300

    00 01

---
+++ Assets/Scripts/BarradeConcentracion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BarradeConcentracion : MonoBehaviour
+{
+    public Slider sliderConcentracion;
+
+    public static float Concentracion;
+    float maxConcentracion=100f;
+
+    void Start()
+    {
+      Concentracion = maxConcentracion;
+    }
+
+    void Update()
+    {
+        sliderConcentracion.value=Concentracion;
+        Concentracion-= 10f* Time.deltaTime ;
+    }
+}
