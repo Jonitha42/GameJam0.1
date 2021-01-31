687fd22810a7acafa1788440bdf1a967dc762493
Jonitha42 <yonipardo4@gmail.com>
2021-01-31 13:11:27 -0300

    13 11

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    
+
+    void Update () {
+     if (Input.GetKey(KeyCode.Space))
+       {   
+            
+           SceneManager.LoadScene(1);
+
+    
+        }
+
+     }
+
+
+}
+
